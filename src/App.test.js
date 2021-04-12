526e553aca4b580c7c6eb584d1d471c08e49bfc5
Joseph Caruana <jcaruana614@gmail.com>
2021-04-12 16:14:25 -0700

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,24 +4,21 @@ import { replaceCamelWithSpaces } from './App';
 
 test('button has correct initial color', () => {
 	render(<App />);
-	// find an element with a role of button and text of 'Change to blue'
 	const colorButton = screen.getByRole('button', { name: 'Change to blue' });
-	// expect the background color to be red
+
 	expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
-	// click button
+
 	fireEvent.click(colorButton);
-	// expect the background color to be blue
+
 	expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
-	// expect the button text to be 'Change to red'
 	expect(colorButton).toHaveTextContent('Change to red');
 });
 
 test('initial conditions', () => {
 	render(<App />);
-	// check button startes enabled
 	const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 	expect(colorButton).toBeEnabled();
-	// check checkbox starts unchecked
+
 	const checkbox = screen.getByRole('checkbox');
 	expect(checkbox).not.toBeChecked();
 });
@@ -60,15 +57,14 @@ test('button turns gray on disable click then back to blue', () => {
 	expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
 });
 
-
 describe('spaces before camel-case capital letters', () => {
 	test('Works for no inner capital letters', () => {
-	  expect(replaceCamelWithSpaces('Red')).toBe('Red');
+		expect(replaceCamelWithSpaces('Red')).toBe('Red');
 	});
 	test('Works for one inner capital letter', () => {
-	  expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+		expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
 	});
 	test('Works for multiple inner capital letters', () => {
-	  expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+		expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
 	});
-  });
+});
